6f534b94341e5086c7be950a5fcb345f84f1fe46
diazleonel928-tech <diazleonel928@gmail.com>
2025-09-26 15:32:40 -0600

    Agregamos mas PHP

---
+++ Practicas-PHP/ConstantesPHP/Constantes.php
@@ -0,0 +1,21 @@
+<?php
+    //Varieble
+    $edad=20;
+
+    echo $edad ."<br/>";
+    //Varieble siempre cambia
+    $edad=40;
+
+    echo $edad ."<br/>";
+
+    //constante
+    define("Nombre","Manuel");
+    define("numeroDeSuerte", 5);
+    const Mensaje=("Hola");
+    const numero=(12);
+
+    echo Mensaje ."<br/>";
+    echo Nombre ."<br/>";
+    echo numeroDeSuerte ."<br/>";
+    echo numero ."<br/>";
+?>
